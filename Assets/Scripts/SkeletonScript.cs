899b703dd6716bc1c8b6c90c0e0158b29641c62d
Dinis Barroso <dinisbarroso.dev@gmail.com>
2024-10-19 16:28:30 +0100

    lápide

--- Assets/Scripts/SkeletonScript.cs
+++ Assets/Scripts/SkeletonScript.cs
@@ -5,8 +5,10 @@ public class SkeletonScript : MonoBehaviour
 {
     public GameObject pref;
     public Rigidbody2D rb2d;
+    private int BonesDropped = 0;
     private IEnumerator SpawnBone() {
         Instantiate(pref, new Vector2(transform.position.x, transform.position.y + .5f), transform.rotation);
+        BonesDropped = BonesDropped + 1;
         yield return new WaitForSeconds(3);
         StartCoroutine(SpawnBone());
     }
@@ -18,10 +20,14 @@ public class SkeletonScript : MonoBehaviour
     void FixedUpdate()
     {
         rb2d.linearVelocity = new Vector2(-2.5f, rb2d.linearVelocity.y);
+        if (BonesDropped == 6) {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.tag == "Bark") {
+            BonesDropped = BonesDropped + 1;
             Instantiate(pref, new Vector2(transform.position.x, transform.position.y + .5f), transform.rotation);
         }
     }
