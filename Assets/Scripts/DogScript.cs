d30b943d4a8d79479e5203e84dd3e6192fe32568
Dinis Barroso <dinisbarroso.dev@gmail.com>
2024-10-19 10:54:33 +0100

    Dog physics

---
+++ Assets/Scripts/DogScript.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class DogScript : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    private float jumpSpeed = 7f;
+    private float walkSpeed = 6f;
+    private bool InGround = true;
+    public SpriteRenderer sr;
+    Vector3 playerVelocity;
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        rb2d.linearVelocity = new Vector2(walkSpeed, rb2d.linearVelocity.y);
+
+        if (Input.GetKey("space") && (InGround == true)){
+            rb2d.linearVelocity = new Vector2(walkSpeed, jumpSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if (col.gameObject.tag == "Wall") {
+            if (walkSpeed == 6f) {
+                walkSpeed = -6f;
+                sr.flipX = true;
+            }
+            else if (walkSpeed == -6f) {
+                walkSpeed = 6f;
+                sr.flipX = false;
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col){
+        if (col.gameObject.tag == "Ground") {
+            InGround = true;
+            rb2d.linearVelocity = new Vector2(walkSpeed, rb2d.linearVelocity.y-1f);
+        }
+    }
+    void OnTriggerExit2D(Collider2D col){
+        if (col.gameObject.tag == "Ground") {
+            InGround = false;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col){
+        if (col.gameObject.tag == "Ground") {
+            InGround = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D col){
+        if (col.gameObject.tag == "Ground") {
+            InGround = false;
+        }
+    }
+}
